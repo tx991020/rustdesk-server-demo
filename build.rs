bd842de309067e4496eaf1287cb417f531efc1c7
tx991020 <1059622332@qq.com>
2021-10-12 12:25:09 +0800

    更新proto

--- build.rs
+++ build.rs
@@ -1,5 +1,7 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::compile_protos("proto/helloworld.proto")?;
+    tonic_build::compile_protos("proto/message.proto")?;
+    tonic_build::compile_protos("proto/rendezvous.proto")?;
 
     Ok(())
 }
--- src/lib.rs
+++ src/lib.rs
@@ -3,12 +3,24 @@
 pub mod hello_world {
     tonic::include_proto!("helloworld");
 }
+
+pub mod rendezvous {
+    tonic::include_proto!("rendezvous");
+}
 pub use hello_world::greeter_server::{Greeter, GreeterServer};
 pub use hello_world::{HelloReply, HelloRequest};
 
+pub mod message {
+    tonic::include_proto!("message");
+}
+pub use message::chatter_server::{Chatter, ChatterServer};
+pub use message::{HelloReply1, HelloRequest1};
+
+pub use rendezvous::register_server::{Register, RegisterServer};
+pub use rendezvous::{HelloReply2, HelloRequest2};
 
 pub mod version;
-pub use version::*;
+pub use version::VERSION;
 pub mod controller;
 
 pub mod config;
